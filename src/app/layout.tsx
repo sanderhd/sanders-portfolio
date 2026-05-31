1c3a0d9a892faa05bbfc08fed31d6e0ca0755833
sanderhd <sanderhoudijk10@gmail.com>
2026-05-31 21:52:55 +0200

    feat: add banner to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,31 @@ const spaceGrotesk = localFont({
 export const metadata: Metadata = {
     title: "Sander's Portfolio",
     description: "Sander's new Portfolio, made with NextJS",
+
+    openGraph: {
+        title: "Sander's Portfolio",
+        description: "Sander's new Portfolio, made with NextJS",
+        url: "https://sander.tf/",
+        siteName: "Sander's Portfolio",
+        images: [
+            {
+                url: "https://sander.tf/banner.png",
+                width: 1200,
+                height: 630,
+                alt: "Sander's Portfolio Banner"
+            },
+        ],
+        type: "website"
+    },
+
+    twitter: {
+        card: "summary_large_image",
+        title: "Sander's Portfolio",
+        description: "Sander's new Portfolio, made with NextJS",
+        images: [
+            "https://sander.tf/banner.png"
+        ],
+    },
 };
 
 export default function RootLayout({
